89082b444fbe9d6efc368d8d1fde254bfce5e0d7
Ghostly Piper <morkelebghost@gmail.com>
2022-06-25 15:08:13 +0300

    D_4

--- simpleapp/views.py
+++ simpleapp/views.py
@@ -1,22 +1,26 @@
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import (
+    ListView, DetailView, CreateView, UpdateView, DeleteView,
+)
+
 from .models import Product
 from .filters import ProductFilter
+from .forms import ProductForm
 
 
 class ProductsList(ListView):
     model = Product
-    ordering = 'name'
-    template_name = 'products.html'
+    ordering = 'name'  # Сортировка по полю
+    template_name = 'products.html'  # Шаблон
     context_object_name = 'products'
-    paginate_by = 2
+    paginate_by = 2  # Пагинация (сколько будет выводиться на странице)
 
     # Переопределяем функцию получения списка товаров
     def get_queryset(self):
         # Получаем обычный запрос
         queryset = super().get_queryset()
         # Используем наш класс фильтрации.
-        # self.request.GET содержит объект QueryDict, который мы рассматривали
-        # в этом юните ранее.
+        # self.request.GET содержит объект QueryDict
         # Сохраняем нашу фильтрацию в объекте класса,
         # чтобы потом добавить в контекст и использовать в шаблоне.
         self.filterset = ProductFilter(self.request.GET, queryset)
@@ -37,3 +41,27 @@ class ProductDetail(DetailView):
     template_name = 'product.html'
     # Название объекта, в котором будет выбранный пользователем продукт
     context_object_name = 'product'
+
+
+# Добавляем новое представление для создания товаров.
+class ProductCreate(CreateView):
+    # Указываем нашу разработанную форму
+    form_class = ProductForm
+    # модель товаров
+    model = Product
+    # и новый шаблон, в котором используется форма.
+    template_name = 'product_edit.html'
+
+
+# Представление обновляющее товар
+class ProductUpdate(UpdateView):
+    form_class = ProductForm
+    model = Product
+    template_name = 'product_edit.html'
+
+
+# Представление удаляющее товар.
+class ProductDelete(DeleteView):
+    model = Product
+    template_name = 'product_delete.html'
+    success_url = reverse_lazy('product_list')
